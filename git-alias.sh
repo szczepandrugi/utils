5a3edb57d8a84d500960629a204b4fe6d56b981f
Szymon Suszko <s.suszko@nowaera.pl>
2017-04-25 11:28:02 +0200

    first

---
+++ git-alias.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MSG="Aliasy Git"
+echo $MSG
+
+git config --global alias.ch checkout && 
+git config --global alias.b branch && 
+git config --global alias.c commit && 
+git config --global alias.s status &&
+git config --global alias.dupa && 
+git config --global alias.f fetch 
