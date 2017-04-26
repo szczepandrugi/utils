485e71ed962601964bebbaafebc4fad1db629f9d
Szymon Suszko <s.suszko@nowaera.pl>
2017-04-26 11:56:07 +0200

    e?

--- git-alias.sh
+++ git-alias.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-<<<<<<< Updated upstream
-
-echo "Aliasy Git"
-
-git config --global alias.ch checkout && 
-git config --global alias.b branch && 
-git config --global alias.c commit && 
-git config --global alias.s status &&
-git config --global alias.dupa && 
-git config --global alias.f fetch 
-=======
-
-echo "Aliasy Git"
 
 git config --global alias.co = checkout && 
 git config --global alias.ch = checkout && 
 git config --global alias.c = commit -m && 
-git config --global alias.ci = commit && 
+git config --global alias.ci = commit -m && 
 git config --global alias.s = status && 
 git config --global alias.st = status && 
+git config --global alias.sh = stash && 
+git config --global alias.shp = stash pop && 
+git config --global alias.shl = stash list && 
 git config --global alias.a = add . && 
 git config --global alias.b = branch && 
-git config --global alias.f = fetch
->>>>>>> Stashed changes
+git config --global alias.f = fetch 
