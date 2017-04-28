e0acd4e52a678699960b0f0ef71d2d19ce0a3c93
Szymon Suszko <s.suszko@nowaera.pl>
2017-04-28 09:50:49 +0200

    remove =

--- git-alias.sh
+++ git-alias.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-git config --global alias.co = checkout && 
-git config --global alias.ch = checkout && 
-git config --global alias.c = commit -m && 
-git config --global alias.ci = commit -m && 
-git config --global alias.s = status && 
-git config --global alias.st = status && 
-git config --global alias.sh = stash && 
-git config --global alias.shp = stash pop && 
-git config --global alias.shl = stash list && 
-git config --global alias.a = add . && 
-git config --global alias.b = branch && 
-git config --global alias.f = fetch 
+git config --global alias.co checkout && 
+git config --global alias.ch checkout && 
+git config --global alias.c commit -m && 
+git config --global alias.ci commit -m && 
+git config --global alias.s status && 
+git config --global alias.st status && 
+git config --global alias.sh stash && 
+git config --global alias.shp stash pop && 
+git config --global alias.shl stash list && 
+git config --global alias.a add . && 
+git config --global alias.b branch && 
+git config --global alias.f fetch 
