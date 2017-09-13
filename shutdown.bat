59469853213729a5efc084278a1b8c67b1244390
Szymon Suszko <sczepandrugi@gmail.com>
2017-09-13 10:16:46 +0200

    Add files via upload

---
+++ shutdown.bat
@@ -0,0 +1,25 @@
+@echo off
+set /p min=Za ile minut? Wpisz 'a' aby anulowac odliczanie. 
+set /a sec=(60*min)
+
+
+IF /I "%min%"=="a" (
+    GOTO ABORT
+) ELSE (
+    GOTO SHUTDOWN
+)
+
+:ABORT
+CLS
+ECHO ABORT
+shutdown /a
+PAUSE
+EXIT
+
+:SHUTDOWN
+CLS
+ECHO SHUTDOWN
+shutdown /a
+PAUSE
+shutdown /t %sec% /s
+EXIT
